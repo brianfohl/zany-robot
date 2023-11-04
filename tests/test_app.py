e6fb10417815b3a5bf7d165e38df336eaa74cafc
Brian Fohl <brian.fohl@gmail.com>
2023-11-04 15:07:24 -0700

    add tests

---
+++ tests/test_app.py
@@ -0,0 +1,29 @@
+import pytest
+from flask import template_rendered
+from contextlib import contextmanager
+from your_flask_app import app  # Import your Flask app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+@contextmanager
+def captured_templates(app):
+    recorded = []
+    def record(sender, template, context, **extra):
+        recorded.append((template, context))
+    template_rendered.connect(record, app)
+    try:
+        yield recorded
+    finally:
+        template_rendered.disconnect(record, app)
+
+def test_hello_world(client):
+    with captured_templates(app) as templates:
+        response = client.get('/')
+        assert response.status_code == 200
+        assert len(templates) == 1
+        template, context = templates[0]
+        assert template.name == "index.html"
+        assert context["title"] == "Hello"
